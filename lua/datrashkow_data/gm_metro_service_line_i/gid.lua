ae3c9529bc5d00595fda7a825c504dcf376d5a56
unknown <premiumcraftbel@gmail.com>
2026-02-12 23:38:40 +0300

    new line - gm_metro_service_line_i

---
+++ lua/datrashkow_data/gm_metro_service_line_i/gid.lua
@@ -0,0 +1,296 @@
+[
+	{
+		"GidTriggerName": "5-1",
+		"Angles": "{0 180.2549 0}",
+		"Pos": "[4334.5073 -15365.8555 -11642.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "5-2",
+		"Angles": "{0 181.6015 0}",
+		"Pos": "[4306.2505 -14994.0439 -11642.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "5-3",
+		"Angles": "{0 182.0502 0}",
+		"Pos": "[4316.2158 -15755.0107 -11642.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "5-3",
+		"Angles": "{0 181.0629 0}",
+		"Pos": "[-2241.7092 -15738.4648 -11642.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "5-1",
+		"Angles": "{0 177.1121 0}",
+		"Pos": "[-2229.8538 -15364.7021 -11642.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "5-2",
+		"Angles": "{0 180.2538 0}",
+		"Pos": "[-2214.1082 -14992.6006 -11642.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "9-1",
+		"Angles": "{0 307.6703 0}",
+		"Pos": "[-265.899 -7323.5718 -15148.4688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "9-1",
+		"Angles": "{0 132.2792 0}",
+		"Pos": "[-3193.5117 -4395.6299 -15148.4688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "10-1",
+		"Angles": "{0 93.0544 0}",
+		"Pos": "[-11057.9648 -3748.813 -15698.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "10-2",
+		"Angles": "{0 90.0025 0}",
+		"Pos": "[-10798.3516 -3800.2568 -15698.3691]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "10-2",
+		"Angles": "{0 88.2971 0}",
+		"Pos": "[-10787.7695 4848.605 -15697.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "10-1",
+		"Angles": "{0 86.4121 0}",
+		"Pos": "[-11057.4141 4895.0317 -15698.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "100-1",
+		"Angles": "{0 189.819 0}",
+		"Pos": "[-1765.5558 -1341.4022 -9140.3496]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "100-2",
+		"Angles": "{0 169.8026 0}",
+		"Pos": "[-1733.9441 -1010.9332 -9140.3496]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "100-2",
+		"Angles": "{0 173.303 0}",
+		"Pos": "[-8669.0889 -1008.7261 -9140.3496]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "100-1",
+		"Angles": "{0 175.8162 0}",
+		"Pos": "[-8678.1797 -1273.2096 -9140.3496]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "100-3",
+		"Angles": "{0 353.4238 0}",
+		"Pos": "[4646.7461 -1332.9099 -9140.9385]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "100-4",
+		"Angles": "{0 355.578 0}",
+		"Pos": "[4622.1221 -1050.1974 -9140.9385]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "2-1",
+		"Angles": "{0 177.9982 0}",
+		"Pos": "[11083.3945 15577.6836 -9533.8691]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "2-1",
+		"Angles": "{0 178.5367 0}",
+		"Pos": "[-1786.3967 15585.2939 -9533.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "2-2",
+		"Angles": "{0 185.0264 0}",
+		"Pos": "[-1703.8646 15313.2686 -9533.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "3-1",
+		"Angles": "{0 77.0746 0}",
+		"Pos": "[-11512.9111 2529.2688 -9963.4688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "3-1",
+		"Angles": "{0 270.121 0}",
+		"Pos": "[-11562.6572 -2633.7126 -9963.4688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "4-1",
+		"Angles": "{0 91.8135 0}",
+		"Pos": "[-13951.3262 47.1716 -10805.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "4-1",
+		"Angles": "{0 89.9286 0}",
+		"Pos": "[-13961.7432 4664.1089 -10805.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "4-D",
+		"Angles": "{0 86.3307 0}",
+		"Pos": "[-13661.9639 -2582.6965 -10806.4688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "101-D",
+		"Angles": "{0 165.5887 0}",
+		"Pos": "[-2951.5662 -14529.8896 -10806.8691]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "101-V",
+		"Angles": "{0 265.9498 0}",
+		"Pos": "[4003.6997 -8695.4912 -10806.8691]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "6-1",
+		"Angles": "{0 355.2586 0}",
+		"Pos": "[-1174.8621 14661.7979 -12068.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "6-1",
+		"Angles": "{0 2.5292 0}",
+		"Pos": "[742.8623 14660.959 -12068.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "6-2",
+		"Angles": "{0 2.0803 0}",
+		"Pos": "[719.0344 14325.0986 -12068.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "6-2",
+		"Angles": "{0 184.8943 0}",
+		"Pos": "[-2293.6924 14205.1309 -12072.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "201-1",
+		"Angles": "{0 13.9292 0}",
+		"Pos": "[-2636.6445 14125.8262 -12072.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "201-2",
+		"Angles": "{0 268.7306 0}",
+		"Pos": "[-6147.5781 4936.6562 -12072.7686]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "7-1",
+		"Angles": "{0 171.9958 0}",
+		"Pos": "[-5022.7124 195.2944 -12930.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "7-1",
+		"Angles": "{0 0.8235 0}",
+		"Pos": "[1046.8916 191.3403 -12930.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "8-1",
+		"Angles": "{0 273.82 0}",
+		"Pos": "[15161.9238 1958.8615 -14314.8691]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "8-1",
+		"Angles": "{0 272.2043 0}",
+		"Pos": "[15163.8584 -6756.1426 -14314.2061]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "8-2",
+		"Angles": "{0 271.1271 0}",
+		"Pos": "[14784.2422 -6481.8335 -14314.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "8-2",
+		"Angles": "{0 90.7096 0}",
+		"Pos": "[14776.1924 1446.1328 -14314.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "11-1",
+		"Angles": "{0 269.585 0}",
+		"Pos": "[13501.998 2241.8438 -15701.8623]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "11-1",
+		"Angles": "{0 270.5724 0}",
+		"Pos": "[13499.2588 -3105.6753 -15709.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "11-2",
+		"Angles": "{0 274.791 0}",
+		"Pos": "[13882.7373 -3051.4253 -15700.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "11-3",
+		"Angles": "{0 272.5471 0}",
+		"Pos": "[14251.2119 -2982.7109 -15699.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "11-3",
+		"Angles": "{0 88.0903 0}",
+		"Pos": "[14254.5684 2301.2271 -15709.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "11-2",
+		"Angles": "{0 90.2446 0}",
+		"Pos": "[13874.4883 2244.2178 -15700.8516]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "11-4",
+		"Angles": "{0 264.2891 0}",
+		"Pos": "[14229.3184 -9119.459 -15699.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "5-4",
+		"Angles": "{0 92.6208 0}",
+		"Pos": "[15584.3828 -7615.6504 -11642.9688]",
+		"SwitchEntityName": ""
+	},
+	{
+		"GidTriggerName": "4-D",
+		"Angles": "{0 175.4624 0}",
+		"Pos": "[-3178.4507 -14546.0459 -10806.8691]",
+		"SwitchEntityName": ""
+	}
+]
